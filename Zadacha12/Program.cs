76e4de5c4f12cf9d4dbb0460bb560135958e8906
Arina M <apelsinka7.ru@yandex.ru>
2022-09-24 22:57:33 +0300

    решение задачи 13

---
+++ Zadacha12/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число ");
+int MyNumber = Convert.ToInt32(Console.ReadLine());
+string MyNumberString = Convert.ToString(MyNumber);
+if (MyNumberString.Length > 2)
+{
+  Console.WriteLine("третья цифра этого числа-> " + MyNumberString[2]);
+}
+else 
+{
+  Console.WriteLine("третьей цифры нет");
+};
